ef32e0e3a2cf3660dd701f30870fdabf69653d81
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2026-04-14 14:06:10 +0200

    Updated dependencies and build script

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,4 @@
 import test from "@chocbite/ts-supplement-eslint";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(test, {
-  rules: {
-    "@typescript-eslint/no-array-delete": "off",
-  },
-});
+export default defineConfig(test, {});
